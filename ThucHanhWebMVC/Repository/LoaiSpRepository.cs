a93b1fe9650a3ac86b87489f26308b2ee1a42f9f
TranHuyHiep <tranhuyhiep0710@gmail.com>
2023-04-03 01:19:55 +0700

    done

---
+++ ThucHanhWebMVC/Repository/LoaiSpRepository.cs
@@ -0,0 +1,41 @@
+﻿using ThucHanhWebMVC.Models;
+namespace ThucHanhWebMVC.Repository
+{
+    public class LoaiSpRepository : ILoaiSpRepository
+    {
+        private readonly QlbanVaLiContext _context;
+        public LoaiSpRepository(QlbanVaLiContext context)
+        {
+            _context = context;
+        }
+
+        public TLoaiSp Add(TLoaiSp loaiSp)
+        {
+            _context.TLoaiSps.Add(loaiSp);
+            _context.SaveChanges();
+            return loaiSp;
+        }
+
+        public TLoaiSp Delete(string maLoaiSp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TLoaiSp> GetAllLoaiSp()
+        {
+            return _context.TLoaiSps;
+        }
+
+        public TLoaiSp GetLoaiSp(string maLoaiSp)
+        {
+            return _context.TLoaiSps.Find(maLoaiSp);
+        }
+
+        public TLoaiSp Update(TLoaiSp loaiSp)
+        {
+            _context.Update(loaiSp);
+            _context.SaveChanges();
+            return loaiSp;
+        }
+    }
+}
